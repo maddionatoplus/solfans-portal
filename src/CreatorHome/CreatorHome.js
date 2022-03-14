38b0899043afbc17ffcf601dffb29b9a8cced005
Alberto <23012066+gentledev@users.noreply.github.com>
2022-03-14 22:47:40 +0100

    implemented basic post preview in dashboard
    - implemented basic post preview in dashboard
    - changed header icons

--- src/CreatorHome/CreatorHome.js
+++ src/CreatorHome/CreatorHome.js
@@ -20,10 +20,8 @@ export default function CreatorHome() {
     setUser,
     setWalletAddress,
     setRefreshData,
-    refreshData
+    refreshData,
   } = useContext(UserContext);
-
- 
 
   const renderSubscriptions = () => {
     console.log(connectedUser);
@@ -52,17 +50,112 @@ export default function CreatorHome() {
     );
   };
 
-  
+  function renderPostPreview(item) {
+    var postPreview;
+    var postPreviewStyle = {
+      height: "80px",
+      width: "120px",
+      borderRadius: "6px",
+      backgroundColor: "#f3f4f6",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      backgroundSize: "cover",
+      backgroundPosition: "center",
+    };
+    if (item.link === "")
+      postPreview = (
+        <div style={postPreviewStyle}>
+          {/* text preview */}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            stroke-width="2"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"
+            />
+          </svg>
+        </div>
+      );
+
+    MyUtil.isImage(item.link)
+      ? (postPreview = (
+          <div
+            style={{
+              ...postPreviewStyle,
+              backgroundImage: "url(" + item.link + ")",
+            }}
+          >
+            {/* image preview */}
+          </div>
+        ))
+      : MyUtil.isVideo(item.link)
+      ? (postPreview = (
+          <div style={postPreviewStyle}>
+            {/* video icon */}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              stroke-width="2"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M7 4v16M17 4v16M3 8h4m10 0h4M3 12h18M3 16h4m10 0h4M4 20h16a1 1 0 001-1V5a1 1 0 00-1-1H4a1 1 0 00-1 1v14a1 1 0 001 1z"
+              />
+            </svg>
+          </div>
+        ))
+      : (postPreview = (
+          <div style={postPreviewStyle}>
+            {/* link icon */}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              stroke-width="2"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1"
+              />
+            </svg>
+          </div>
+        ));
+
+    return (
+      <Link to="/post/view" state={{ post: item, creator: connectedUser }}>
+        {postPreview}
+      </Link>
+    );
+  }
+
   const renderPostList = () => {
     return (
-      <ul className="divide-y-2 divide-gray-100 overflow-x-auto w-full" style={{marginTop: 0}}>
+      <ul
+        className="divide-y-2 divide-gray-100 overflow-x-auto w-full"
+        style={{ marginTop: 0 }}
+      >
         {connectedUser.contents
           .slice(0)
           .reverse()
           .map((item, index) => {
             return (
-              <li className="pb-6 pt-6 flex justify-between text-base text-gray-500 font-semibold">
-                <p className="px-4 text-gray-800">
+              <li className="pb-6 pt-6 flex items-center text-base text-gray-500 font-semibold">
+                {renderPostPreview(item)}
+                <p className="px-4 text-gray-800 flex-auto">
                   {item.title}
                   <br />
                   <span
@@ -98,24 +191,28 @@ export default function CreatorHome() {
                         />
                       </svg>
                     </Link>
-                    }
-                     {
-                        <a href="#" onClick={()=> showDeleteClick(item)} className="mr-2 hover:text-indigo-600">
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="h-6 w-6"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
-                            >
-                            <path
-                              strokeLinecap="round"
-                              stroke-linejoin="round"
-                              stroke-width="2"
-                              d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
-                              />
-                          </svg>
-                        </a>                      
+                  }
+                  {
+                    <a
+                      href="#"
+                      onClick={() => showDeleteClick(item)}
+                      className="mr-2 hover:text-indigo-600"
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="h-6 w-6"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          stroke-linejoin="round"
+                          stroke-width="2"
+                          d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+                        />
+                      </svg>
+                    </a>
                   }
                 </p>
               </li>
@@ -136,17 +233,26 @@ export default function CreatorHome() {
                   RECENT POSTS
                 </span>
               </div>
-              {
-              connectedUser.contents.length ? renderPostList() : 
-                <div style={{padding: "50px 0px"}} className="bg-white col-span-3 p-6 mx-4 rounded-xl border border-gray-50 flex flex-col space-y-6 mb-6 items-center">
+              {connectedUser.contents.length ? (
+                renderPostList()
+              ) : (
+                <div
+                  style={{ padding: "50px 0px" }}
+                  className="bg-white col-span-3 p-6 mx-4 rounded-xl border border-gray-50 flex flex-col space-y-6 mb-6 items-center"
+                >
                   <span className="text-2xl text-gray-500 font-semibold">
                     You haven't create any posts yet.
                   </span>
-                    <div className="content-around">
-                        <Link to="/post/new" className="px-4 py-2 m-10 text-sm bg-indigo-700 text-white rounded uppercase tracking-wider text-center font-semibold hover:bg-indigo-600 hover:text-indigo-100">Create your first post</Link>
-                    </div>
+                  <div className="content-around">
+                    <Link
+                      to="/post/new"
+                      className="px-4 py-2 m-10 text-sm bg-indigo-700 text-white rounded uppercase tracking-wider text-center font-semibold hover:bg-indigo-600 hover:text-indigo-100"
+                    >
+                      Create your first post
+                    </Link>
+                  </div>
                 </div>
-              }
+              )}
             </div>
             <div className="col-span-1 bg-white p-6 rounded-xl border border-gray-50 flex flex-col space-y-6">
               <div className="flex justify-between items-center">
@@ -224,22 +330,19 @@ export default function CreatorHome() {
     );
   };
 
-
-
   const renderNotConnectedUser = () => {
     return (
       <div className="flex flex-col mx-4 space-y-8 pt-3">
         <div className="container col-span-2">
-            <br/>
+          <br />
           <div className="bg-white p-6 rounded-xl border border-gray-50 flex flex-col space-y-6 mb-6 items-center">
-          <img src={solfansLogo} alt="solfans" width={200}></img>
+            <img src={solfansLogo} alt="solfans" width={200}></img>
             <span className="text-2xl text-gray-500 font-semibold">
-              Become a Creator and start earning SOL from your
-              community!
+              Become a Creator and start earning SOL from your community!
             </span>
             {drawNoConnectedButton()}
-            <br/>
-            <br/>
+            <br />
+            <br />
           </div>
         </div>
       </div>
@@ -312,36 +415,34 @@ export default function CreatorHome() {
   };
 
   const showDeleteClick = (item) => {
-    if (window.confirm('Are you sure you want to delete this post?')) {
-      deleteContent(item)
-    } 
-  }
-  
+    if (window.confirm("Are you sure you want to delete this post?")) {
+      deleteContent(item);
+    }
+  };
+
   const deleteContent = async (item) => {
     try {
-        const index = connectedUser.contents.indexOf(item)
+      const index = connectedUser.contents.indexOf(item);
 
-        const provider = getProvider();
-        console.log(provider);
-        const program = new Program(idl, programID, provider);
-        
-        console.log(baseAccount.publicKey)
-        console.log(walletAddress) 
+      const provider = getProvider();
+      console.log(provider);
+      const program = new Program(idl, programID, provider);
 
-        await program.rpc.deleteContent(
-            index, {
-            accounts: {
-                baseAccount: baseAccount.publicKey,
-                user: walletAddress,
-            },
-        }); 
+      console.log(baseAccount.publicKey);
+      console.log(walletAddress);
 
-        setRefreshData(!refreshData)
+      await program.rpc.deleteContent(index, {
+        accounts: {
+          baseAccount: baseAccount.publicKey,
+          user: walletAddress,
+        },
+      });
 
+      setRefreshData(!refreshData);
     } catch (error) {
-        console.log("Error uploading content:", error)
+      console.log("Error uploading content:", error);
     }
-  }; 
+  };
 
   const deletePostModal = () => {
     // if(itemToDelete){
@@ -351,7 +452,7 @@ export default function CreatorHome() {
     //           <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
     //               <div className="flex justify-end p-2">
     //                   <button type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white">
-    //                       <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>  
+    //                       <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
     //                   </button>
     //               </div>
     //               <div className="p-6 pt-0 text-center">
@@ -369,8 +470,8 @@ export default function CreatorHome() {
     //     </div>
     //   );
     // }
-  }; 
-   
+  };
+
   return (
     <div className="relative antialiased bg-gray-100 min-h-screen">
       {walletAddress && Menu(connectedUser, false, true)}
